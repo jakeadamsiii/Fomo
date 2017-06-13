e757f037d75e5570a107add124df0b69b1008d73
Jake Adams <jadams.html@gmail.com>
2017-06-13 21:26:35 +0100

    started adding fixed header functinality

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,18 @@
 angular
   .module('instagramApp', ['ui.router', 'ngResource', 'satellizer', 'ngMessages']);
+
+
+  window.onscroll = changePos;
+
+  function changePos() {
+      var header = document.getElementById("header");
+      if (window.pageYOffset > 100) {
+          header.style.position = "fixed";
+          header.style.top = "0";
+          header.style.right = "0";
+          header.style.left = "0";
+      } else {
+          header.style.position = "";
+          header.style.top = "";
+      }
+  }
